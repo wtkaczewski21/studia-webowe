cda146ae52c63d62a68883867e4d2e1f16b4240d
wtkaczewski <wojtalokt@gmail.com>
2023-06-11 16:32:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuButton = document.getElementsByClassName("menu-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+menuButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("picture");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
